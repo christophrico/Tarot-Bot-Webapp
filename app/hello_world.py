bfbaa452589b1c6fa477bfa6f353778298c86d7a
Christopher Rico <christophrico@users.noreply.github.com>
2020-10-30 00:50:52 -0700

    Change to make ready for gcloud

--- app/hello_world.py
+++ app/hello_world.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, world!"
+    return "Hello, world! This is neat!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=8080,
-            debug=True)
+    app.run()
