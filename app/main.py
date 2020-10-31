167a08d46b0c8e56b5bbf3e4540449ff56c6b5d6
Christopher Rico <christophrico@users.noreply.github.com>
2020-10-31 00:02:11 -0700

    main.py now serving out index.html, added image upload script and css

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, world! This is neat!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True, use_reloader=True)
